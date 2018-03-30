33ee7d09c14379f736dc355a0a7de6fbf65880c6
Evan Tran <evant179@gmail.com>
2018-03-29 23:55:26 -0700

    update log to test travis deployment

--- service-secret-santa-generator/app.js
+++ service-secret-santa-generator/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('secret santa generator service works!');
+  res.send('secret santa generator service works!!!!');
 });
 
 module.exports.handler = serverless(app);
