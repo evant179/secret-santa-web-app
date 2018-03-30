93c6624e77e97fb7d50f5621eec21c91d2d61172
Evan Tran <evant179@gmail.com>
2018-03-29 22:31:27 -0700

    add example mocha unit test

---
+++ service-secret-santa-generator/test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('ExampleTest', function () {
+  describe('#exampleTest()', function () {
+    it('running example unit test', function () {
+      assert.equal(1, 1);
+    });
+  });
+});
