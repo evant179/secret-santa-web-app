1ac57d74e02c7b2175a2777ef142a56d46b7d03b
Evan Tran <evant179@gmail.com>
2018-04-01 21:47:39 -0700

    set up unit test framework

---
+++ service-secret-santa-generator/test/secret-santa.test.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const mock = require('sinon-express-mock');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const expect = chai.expect;
+chai.use(sinonChai);
+
+const secretsanta = require('../app/controllers/secret-santa.controller');
+
+describe('secret-santa.controller tests', function () {
+  describe('#generate', function () {
+    it('generates a secret santa per attendee', function () {
+      let req = createRequest();
+      let res = mock.mockRes();
+      secretsanta.generate(req, res);
+      expect(res.status).to.be.calledWith(200);
+      expect(res.status().send).to.be.calledWith({ name: "testName" });
+    });
+  });
+});
+
+function createRequest() {
+  let req = {};
+  let body = {
+    attendees: [
+      {
+        name: "name1"
+      },
+      {
+        name: "name2"
+      }
+    ]
+  };
+
+  req['body'] = body;
+  return req;
+}
--- service-secret-santa-generator/test/test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-
-describe('ExampleTest', function () {
-  describe('#exampleTest()', function () {
-    it('running example unit test', function () {
-      assert.equal(1, 1);
-    });
-  });
-});
