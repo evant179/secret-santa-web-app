3d0aeadb02616fe09aa44422f0a03117e11aedcc
Evan Tran <evant179@gmail.com>
2018-04-08 15:53:36 -0700

    verify unique attendees in generate and handle error

--- service-secret-santa-generator/app/controllers/secret-santa.controller.js
+++ service-secret-santa-generator/app/controllers/secret-santa.controller.js
@@ -1,10 +1,15 @@
-var _ = require("underscore");
+const BadRequestError = require("../errors/bad-request-error");
+const validator = require("../helpers/validator");
+const _ = require("underscore");
 
 exports.generate = function (req, res) {
     try {
         console.log('Enter generate');
         console.log(req.body);
         let attendees = req.body;
+
+        // verify request data
+        validator.verifyUniqueAttendees(attendees);
 
         // initialize map used to keep track of available names for selection
         let availableNames = createAvailableNameList(attendees);
@@ -20,7 +25,8 @@ exports.generate = function (req, res) {
         }
         catch (e) {
             console.log(e);
-            return res.status(406).send({ error: e.message, stack: e.stack })
+            let error = { error: e.message, stack: e.stack };
+            return res.status(406).send(error);
         }
 
         console.log('Exit generate');
@@ -29,7 +35,12 @@ exports.generate = function (req, res) {
     }
     catch (e) {
         console.log(e);
-        return res.status(500).send({ error: e.message, stack: e.stack })
+        let error = { error: e.message, stack: e.stack };
+        if (e instanceof BadRequestError) {
+            return res.status(400).send(error);
+        } else {
+            return res.status(500).send(error)
+        }
     }
 };
 
@@ -50,7 +61,8 @@ function assignSelectedName(attendee, availableNames) {
     remove(namePool, attendee.name);
 
     if (namePool.length < 1) {
-        let message = `[${attendee.name}] has no more names left in their remaining name pool!`;
+        let message = `Secret santa generation has stopped! Please try again. ` +
+            `Reason: [${attendee.name}] has no more names left in their remaining name pool!`;
         throw new Error(message);
     }
 
