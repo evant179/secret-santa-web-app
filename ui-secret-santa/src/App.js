c08a26e08e925421e27521e0c289ba2887db504c
Evan Tran <evant179@gmail.com>
2018-04-21 22:51:37 -0700

    update main page to include 'coming soon' logo

--- ui-secret-santa/src/App.js
+++ ui-secret-santa/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/yoshi-cat.jpg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Coming Soon!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo main-logo" alt="logo" />
       </div>
     );
   }
